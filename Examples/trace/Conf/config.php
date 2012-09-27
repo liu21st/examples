a0dd9561a3fc3a51a94e2aa378cc0a81204002a6
thinkphp <thinkphp@qq.com>
2012-09-27 17:12:15 +0800

    示例的配置文件格式规范

--- Examples/trace/Conf/config.php
+++ Examples/trace/Conf/config.php
@@ -1,12 +1,12 @@
 <?php
 return array(
-    'URL_MODEL'=>2, // 如果你的环境不支持PATHINFO 请设置为3
-    'DB_TYPE'=>'mysql',
-    'DB_HOST'=>'localhost',
-    'DB_NAME'=>'examples',
-    'DB_USER'=>'root',
-    'DB_PWD'=>'',
-    'DB_PORT'=>'3306',
-    'DB_PREFIX'=>'think_',
-    'SHOW_PAGE_TRACE'=>1,
+    'URL_MODEL'			=>	2, // 如果你的环境不支持PATHINFO 请设置为3
+    'DB_TYPE'			=>	'mysql',
+    'DB_HOST'			=>	'localhost',
+    'DB_NAME'			=>	'examples',
+    'DB_USER'			=>	'root',
+    'DB_PWD'			=>	'',
+    'DB_PORT'			=>	'3306',
+    'DB_PREFIX'			=>	'think_',
+    'SHOW_PAGE_TRACE'	=>	1,
 );
